867bb1523a7594300fe79bc824594dde0e7266a6
Alex Norell <alex@alexnorell.com>
2017-11-06 16:27:20 -0800

    Testing namespaces.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "lib/lab06/inc/deck.h"
+#include "lib/lab07/inc/tree.h"
 
 int main() {
-    //deck newdeck;
+    lab6::deck newdeck = lab6::deck();
+    lab7::tree test_tree = lab7::tree();
     return 0;
 }
