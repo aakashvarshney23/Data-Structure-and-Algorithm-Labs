5d5d2f2aa629de1c5d878a8eb5546ef6cad45672
Alex Norell <alex@alexnorell.com>
2017-11-14 13:34:54 -0800

    Added insertion sort and refactored a couple of the sorting functions.

--- lib/lab08/inc/sorts.h
+++ lib/lab08/inc/sorts.h
@@ -9,8 +9,10 @@ public:
     lab6::doubly_linked_list selection_sort(lab6::doubly_linked_list input, int iterations);
     lab6::doubly_linked_list bubble_sort(lab6::doubly_linked_list input, int iterations);
     lab6::doubly_linked_list cocktail_sort(lab6::doubly_linked_list input, int iterations);
-    lab6::doubly_linked_list merge_sort(lab6::doubly_linked_list input, int iterations);
-    lab6::doubly_linked_list quick_sort(lab6::doubly_linked_list input, int iterations);
+
+    lab6::doubly_linked_list quick_sort(lab6::doubly_linked_list input);
+    lab6::doubly_linked_list merge_sort(lab6::doubly_linked_list input);
+    lab6::doubly_linked_list radix_sort(lab6::doubly_linked_list input);
 };
 
 #endif //CMPE126F17_LABS_SORTS_H
--- lib/lab08/src/sorts.cpp
+++ lib/lab08/src/sorts.cpp
@@ -1,11 +1,39 @@
 #include "sorts.h"
 
 lab6::doubly_linked_list sorts::insertion_sort(lab6::doubly_linked_list input, int iterations) {
-    return lab6::doubly_linked_list();
+    lab6::doubly_linked_list working_list = input;
+    if(iterations > working_list.get_size()) iterations = working_list.get_size();
+    for(unsigned iteration = 1; iteration <= iterations; iteration++)
+    {
+        unsigned compare_location = iteration-1;
+        while(compare_location >= 0)
+        {
+            if(compare_location == 0)
+            {
+                working_list.swap(compare_location, iteration);
+                break;
+            }
+            else if(working_list.get_data(iteration) < working_list.get_data(compare_location))
+            {
+                compare_location--;
+            }
+            else{
+                working_list.swap(compare_location, iteration);
+                break;
+            }
+        }
+    }
+    return working_list;
 }
 
 lab6::doubly_linked_list sorts::selection_sort(lab6::doubly_linked_list input, int iterations) {
-    return lab6::doubly_linked_list();
+    lab6::doubly_linked_list working_list = input;
+    if(iterations > working_list.get_size()) iterations = working_list.get_size();
+    for(unsigned iteration = 1; iteration <= iterations; iteration++)
+    {
+
+    }
+    return working_list;
 }
 
 lab6::doubly_linked_list sorts::bubble_sort(lab6::doubly_linked_list input, int iterations) {
@@ -16,10 +44,14 @@ lab6::doubly_linked_list sorts::cocktail_sort(lab6::doubly_linked_list input, in
     return lab6::doubly_linked_list();
 }
 
-lab6::doubly_linked_list sorts::merge_sort(lab6::doubly_linked_list input, int iterations) {
+lab6::doubly_linked_list sorts::quick_sort(lab6::doubly_linked_list input) {
     return lab6::doubly_linked_list();
 }
 
-lab6::doubly_linked_list sorts::quick_sort(lab6::doubly_linked_list input, int iterations) {
+lab6::doubly_linked_list sorts::merge_sort(lab6::doubly_linked_list input) {
+    return lab6::doubly_linked_list();
+}
+
+lab6::doubly_linked_list sorts::radix_sort(lab6::doubly_linked_list input) {
     return lab6::doubly_linked_list();
 }
